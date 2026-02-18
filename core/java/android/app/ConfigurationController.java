4d0386a3d5a26dbae47eac4b9dd41775389a532f
NurKeinNeid <nurkeinneid@derpfest.org>
2026-02-18 18:55:46 +0100

    Revert "graphics: Override system fonts with user-selected overlays"
    This reverts commit 7c122a0169aca47a3f0d824eb91da7c31e5e62b5.

--- core/java/android/app/ConfigurationController.java
+++ core/java/android/app/ConfigurationController.java
@@ -29,7 +29,6 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.HardwareRenderer;
-import android.graphics.Typeface;
 import android.os.LocaleList;
 import android.os.Trace;
 import android.util.DisplayMetrics;
@@ -199,7 +198,6 @@ class ConfigurationController {
 
             final Application app = mActivityThread.getApplication();
             final Resources appResources = app.getResources();
-            Typeface.updateDefaultFont(appResources);
             mResourcesManager.applyConfigurationToResources(config, compat);
             updateLocaleListFromAppContext(app.getApplicationContext());
 
